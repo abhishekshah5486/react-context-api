30df6754e750a39787fcac977aacda2197433d00
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-28 20:32:00 +0530

    Created UserContext and Provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Profile from './Components/Profile';
+import Login from './Components/Login';
+import UserContextProvider from './Context/UserContextProvider';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <UserContextProvider>
+      <div>
+        <h1>React Context API</h1>
+        <Login/>
+        <Profile/>
+      </div>
+    </UserContextProvider>
   );
 }
 
